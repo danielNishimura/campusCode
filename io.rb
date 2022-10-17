cbed557deec0bca8a6c1acc5a6e570ce943c5fa8
danielNishimura <nishimurani@gmail.com>
2022-10-17 14:14:10 -0300

    add io and variable filee

---
+++ io.rb
@@ -0,0 +1,10 @@
+#saida de dados
+puts 'Bem vindo ao cookBook sua rede social de receitas'
+
+puts 'Digite o nome da receita'
+
+
+#entrada de dados
+gets
+#saida
+puts 'Receita cadastrada com sucesso!'
---
+++ variaveis.rb
@@ -0,0 +1,10 @@
+#usando variaveis
+numero = 10/5
+puts numero
+
+puts 'Qual seu primeiro nome?'
+nome = gets.chomp()
+puts 'Qual o seu sobrenome'
+sobrenome = gets.chomp()
+#puts "Meu nome é " + nome + " e o meu sobrenome é " + sobrenome
+puts "Meu nome é #{nome} e meu sobrenome é #{sobrenome}"
